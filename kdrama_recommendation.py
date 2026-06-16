ee42f12d86252b41e582f437e2f4cc30ceec8732
bhavyasenthilkumar2007-droid <bhavyasenthilkumar2007@gmail.com>
2026-06-16 13:57:19 +0530

    Create kdrama_recommendation.py

---
+++ kdrama_recommendation.py
@@ -0,0 +1,35 @@
+print("=== K-Drama Recommendation System ===")
+
+while True:
+    drama = input("Enter your favourite drama: ").lower()
+
+    if drama == "true beauty":
+        print("Recommended Drama: My ID is Gangnam Beauty")
+
+    elif drama == "queen of tears":
+        print("Recommended Drama: Crash Landing on You")
+
+    elif drama == "business proposal":
+        print("Recommended Drama: What's Wrong with Secretary Kim")
+
+    elif drama == "lovely runner":
+        print("Recommended Drama: Twinkling Watermelon")
+
+    elif drama == "crash landing on you":
+        print("Recommended Drama: Descendants of the Sun")
+
+    elif drama == "goblin":
+        print("Recommended Drama: Hotel Del Luna")
+
+    elif drama == "my demon":
+        print("Recommended Drama: Doom at Your Service")
+
+    elif drama == "marry my husband":
+        print("Recommended Drama: Perfect Marriage Revenge")
+
+    elif drama == "exit":
+        print("Thank you for using K-Drama Recommendation System!")
+        break
+
+    else:
+        print("Recommended Drama: Strong Woman Do Bong Soon")
